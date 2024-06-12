1559e181227de830813042c76808bf9fcc26705d
fadelanumah <fadelanumah@gmail.com>
2024-06-13 01:55:36 +0700

    feat(admin-dashboard-index): Add Components Implemtation

---
+++ resources/views/includes/admin/foot.blade.php
@@ -0,0 +1,8 @@
+<script src="{{ asset('static/js/components/dark.js') }}"></script>
+<script src="{{ asset('extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+
+<script src="{{ asset('compiled/js/app.js') }}"></script>
+
+<!-- Need: Apexcharts -->
+<script src="{{ asset('extensions/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('static/js/pages/dashboard.js') }}"></script>
