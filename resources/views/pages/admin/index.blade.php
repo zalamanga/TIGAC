c257ad3387ffc27ed7cec2117f4a3f2762d4cbd5
fadelanumah <fadelanumah@gmail.com>
2024-06-13 21:26:15 +0700

    feat(admin-dashboard): Add Profile Card

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -10,5 +10,21 @@
               icon="bi bi-stack"
           ></x-profile-statistics>
         </div>
+      </div>
+      <div class="col-12 col-lg-3">
+              <div class="card">
+                <div class="card-body py-4 px-4">
+                  <div class="d-flex align-items-center">
+                    <div class="avatar avatar-xl">
+                      <img src="{{ asset('compiled/jpg/1.jpg') }}" alt="Face 1" />
+                    </div>
+                    <div class="ms-3 name">
+                      <h5 class="font-bold">John Duck</h5>
+                      <h6 class="text-muted mb-0">@johnducky</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+      </div>
     </section>
 @endsection
