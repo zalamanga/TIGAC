45a1471de9cf28a4b61e6d543ab657565d904f0a
fadelanumah <fadelanumah@gmail.com>
2024-06-13 11:18:28 +0700

    feat(authentication): Add Register Page

---
+++ resources/views/pages/auth/register.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.auth')
+@push('styles')
+    <link rel="stylesheet" crossorigin href="{{ asset('compiled/css/auth.css') }}" />
+@endpush
+@section('content')
+    <div class="row h-100">
+        <div class="col-lg-5 col-12">
+            <div id="auth-left">
+                <div class="auth-logo">
+                    <a href="#" class="navbar-brand"><img src="{{ asset('images/logo.png') }}" alt="TIGAC logo"
+                            srcset="" /></a>
+                </div>
+                <h1 class="auth-title">Sign Up</h1>
+                <p class="auth-subtitle mb-5">
+                    Input your data to register to our website.
+                </p>
+
+                <form action="{{ route('register') }}" method="POST">
+                    @csrf
+
+                    <div class="form-group position-relative has-icon-left mb-4">
+                        <input type="text" class="form-control form-control-xl" placeholder="Email" name="email"
+                            value="{{ old('email') }}" />
+                        <div class="form-control-icon">
+                            <i class="bi bi-envelope"></i>
+                        </div>
+                        @error('email')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group position-relative has-icon-left mb-4">
+                        <input type="text" class="form-control form-control-xl" placeholder="Username" name="name"
+                            value="{{ old('name') }}" />
+                        <div class="form-control-icon">
+                            <i class="bi bi-person"></i>
+                        </div>
+                        @error('name')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group position-relative has-icon-left mb-4">
+                        <input type="password" class="form-control form-control-xl" placeholder="Password" name="password"
+                            value="{{ old('password') }}" />
+                        <div class="form-control-icon">
+                            <i class="bi bi-shield-lock"></i>
+                        </div>
+                        @error('password')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group position-relative has-icon-left mb-4">
+                        <input type="password" class="form-control form-control-xl" placeholder="Confirm Password" name="password_confirmation" />
+                        <div class="form-control-icon">
+                            <i class="bi bi-shield-lock"></i>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">
+                        Sign Up
+                    </button>
+                </form>
+                <div class="text-center mt-5 text-lg fs-4">
+                    <p class="text-gray-600">
+                        Already have an account?
+                        <a href="auth-login.html" class="font-bold">Log in</a>.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-7 d-none d-lg-block">
+            <div id="auth-right"></div>
+        </div>
+    </div>
+@endsection
