40a3a2082293cb7582dc993386474f979502e91c
fadelanumah <fadelanumah@gmail.com>
2024-06-13 22:15:02 +0700

    fix(admin-dashboard): Add Profile Card

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -19,8 +19,8 @@
                       <img src="{{ asset('compiled/jpg/1.jpg') }}" alt="Face 1" />
                     </div>
                     <div class="ms-3 name">
-                      <h5 class="font-bold">John Duck</h5>
-                      <h6 class="text-muted mb-0">@johnducky</h6>
+                      <h5 class="font-bold">{{ $loggedUserData->name }}</h5>
+                      <h6 class="text-muted mb-0">{{ $loggedUserData->email }}</h6>
                     </div>
                   </div>
                 </div>
