5a38f14c53e414b835c70692e804155e0945b1e8
fadelanumah <fadelanumah@gmail.com>
2024-06-18 14:26:59 +0700

    feat(product-category): Add Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,9 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@gmail.com',
             'password' => static::$password ??= Hash::make('admin123'),
         ]);
+
+        $this->call([
+            ProductCategorySeeder::class
+        ]);
     }
 }
