c1f55581691e229d78d5e8ace1d3a703a1e73a0b
fadelanumah <fadelanumah@gmail.com>
2024-06-18 10:02:57 +0700

    feat(dashboard): Add Admin Profile Card to Sidebar

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -39,6 +39,17 @@
             </div>
         </div>
         <div class="sidebar-menu">
+            <div class="card-body py-4 px-4">
+                <div class="d-flex align-items-center">
+                    <div class="avatar avatar-xl">
+                        <img src="{{ asset('compiled/jpg/1.jpg') }}" alt="Face 1" />
+                    </div>
+                    <div class="ms-3 name overflow-hidden">
+                        <h5 class="font-bold">{{ $loggedUserData->name }}</h5>
+                        <h6 class="text-muted mb-0">{{ $loggedUserData->email }}</h6>
+                    </div>
+                </div>
+            </div>
             <ul class="menu">
                 <li class="sidebar-title">Profile Setting</li>
 
--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -1,30 +1,17 @@
 @extends('layouts.admin.main')
 @section('content')
     <section class="row">
-      <div class="col-12 col-lg-9">
-        <div class="row">
-          <x-profile-statistics
-              title="Total Products"
-              statistics="10.000"
-              colorTheme="purple"
-              icon="bi bi-stack"
-          ></x-profile-statistics>
+        <div class="col-12 col-lg-12">
+            <div class="row">
+                <x-profile-statistics title="Total Products" statistics="10.000" colorTheme="purple"
+                    icon="bi bi-stack"></x-profile-statistics>
+                <x-profile-statistics title="Total Products" statistics="10.000" colorTheme="purple"
+                    icon="bi bi-stack"></x-profile-statistics>
+                <x-profile-statistics title="Total Products" statistics="10.000" colorTheme="purple"
+                    icon="bi bi-stack"></x-profile-statistics>
+                <x-profile-statistics title="Total Products" statistics="10.000" colorTheme="purple"
+                    icon="bi bi-stack"></x-profile-statistics>
+            </div>
         </div>
-      </div>
-      <div class="col-12 col-lg-3">
-              <div class="card">
-                <div class="card-body py-4 px-4">
-                  <div class="d-flex align-items-center">
-                    <div class="avatar avatar-xl">
-                      <img src="{{ asset('compiled/jpg/1.jpg') }}" alt="Face 1" />
-                    </div>
-                    <div class="ms-3 name">
-                      <h5 class="font-bold">{{ $loggedUserData->name }}</h5>
-                      <h6 class="text-muted mb-0">{{ $loggedUserData->email }}</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-      </div>
     </section>
 @endsection
