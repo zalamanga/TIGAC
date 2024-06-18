8ae41e43d8f70247987da179ca0617c20f98940d
fadelanumah <fadelanumah@gmail.com>
2024-06-18 17:43:03 +0700

    feat(admin-dashboard): Add JQuery and Datatables Package

--- resources/views/includes/admin/foot.blade.php
+++ resources/views/includes/admin/foot.blade.php
@@ -1,8 +1,9 @@
+
 <script src="{{ asset('static/js/components/dark.js') }}"></script>
 <script src="{{ asset('extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
 
 <script src="{{ asset('compiled/js/app.js') }}"></script>
 
-<!-- Need: Apexcharts -->
-<script src="{{ asset('extensions/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('static/js/pages/dashboard.js') }}"></script>
+<script src="{{ asset('extensions/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('extensions/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{ asset('extensions/datatables.net-bs5/js/dataTables.bootstrap5.min.js')}}"></script>
