f530a41beba3a9f0673e998f6697bd8af8d007d5
fadelanumah <fadelanumah@gmail.com>
2024-06-19 13:13:29 +0700

    feat(product-category): Add Create Product Category View

---
+++ resources/views/pages/admin/product-category/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin.main')
+@section('content')
+    <div class="card">
+        <div class="card-content">
+            <div class="card-body">
+                <form class="form form-vertical">
+                    <div class="form-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="first-name-vertical">Name</label>
+                                    <input type="text" id="first-name-vertical" class="form-control" name="name"
+                                        placeholder="Category Name" data-parsley-required="true" required >
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="email-id-vertical">Description</label>
+                                    <textarea type="text" id="email-id-vertical" class="form-control" name="description"
+                                        placeholder="Category Description"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class='form-check'>
+                                    <div class="checkbox">
+                                        <input type="checkbox" id="checkbox3" class='form-check-input' data-parsley-required="true" required>
+                                        <label for="checkbox3">Data yang diinput sudah benar</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
+                                <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -61,4 +61,11 @@ Route::get('/admin/products', function () {
     return "Product List";
 })->name('admin.product');
 
-Route::get('/admin/products/categories', [ProductCategoryController::class, 'index'])->name('admin.product.product-category');
+Route::get('/admin/products/categories', [ProductCategoryController::class, 'index'])->name('admin.product.product-category.index');
+Route::get('/admin/products/categories/store', function () {
+    $title = 'Create New Product Category';
+    
+    return view('pages.admin.product-category.show', [
+        'title' => $title
+    ]);
+})->name('admin.product.product-category.create');
