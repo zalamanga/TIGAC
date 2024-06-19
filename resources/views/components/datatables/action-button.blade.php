e62a015d44ea2afcdbf203ed8912f7edf7d36c6b
fadelanumah <fadelanumah@gmail.com>
2024-06-19 15:21:29 +0700

    feat(product-category-datatable): Add Action Column

---
+++ resources/views/components/datatables/action-button.blade.php
@@ -0,0 +1,11 @@
+<div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle me-1" type="button"
+        id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true"
+        aria-expanded="false">
+        Action
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item text-danger" href="#">Delete</a>
+        <a class="dropdown-item" href="#">Edit</a>
+    </div>
+</div>
