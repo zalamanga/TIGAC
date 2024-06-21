2adb6e0e6d53aaeeebdeb10eb2f23aec99ccfe89
fadelanumah <fadelanumah@gmail.com>
2024-06-21 14:37:11 +0700

    feat(product-variant): Add Create Request

---
+++ app/Http/Requests/ProductVariantRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductVariantRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'unique:variants,name|required|max:50',
+            'description' => 'nullable|max:255',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => "Product category name is required",
+            'name.unique' => "Product category already exists"
+        ];
+    }
+}
