720b0b36e68d5430aa26adf6c7729182d5d055d7
Ardianto Nugroho <ardi19nugroho@gmail.com>
2024-06-27 10:08:27 +0700

    style: retag brand item at brand section can linked

--- resources/views/components/sections/brands.blade.php
+++ resources/views/components/sections/brands.blade.php
@@ -1,29 +1,29 @@
 <div>
     <h3 class="text-center display-5 fw-bold text-white mb-5">PRODUCT WE’VE ALREADY HELPED LEAVE A MARK</h3>
-    <div class="overflow-hidden">>
+    <div class="overflow-hidden">
         <div class="d-flex align-items-center mb-3 gap-5 brand-container">
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">HOMPIMPA</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">SAMOSE</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">301 ALLIAN</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Dead Squad</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">TIGI</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Jaxel</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Treev</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Slank Up</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">21 Juice Project</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">TOXIC</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Lawless</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">HOMPIMPA</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">SAMOSE</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">301 ALLIAN</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Dead Squad</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">TIGI</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Jaxel</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Treev</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Slank Up</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">21 Juice Project</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">TOXIC</h5>
-            <h5 class="fs-1 fw-semibold brand-item text-nowrap">Lawless</h5>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">HOMPIMPA</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">SAMOSE</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">301 ALLIAN</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Dead Squad</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">TIGI</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Jaxel</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Treev</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Slank Up</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">21 Juice Project</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">TOXIC</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Lawless</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">HOMPIMPA</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">SAMOSE</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">301 ALLIAN</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Dead Squad</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">TIGI</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Jaxel</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Treev</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Slank Up</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">21 Juice Project</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">TOXIC</a>
+            <a href="#" class="fs-1 fw-semibold text-decoration-none brand-item text-nowrap">Lawless</a>
         </div>
     </div>
 </div>
