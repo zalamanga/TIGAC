f91b371bb0723699984a43f244581580b541c0fc
fadelanumah <fadelanumah@gmail.com>
2024-08-01 11:13:09 +0700

    feat(hero-banner): Add Hero Banner Index

---
+++ app/DataTables/HeroBannerDataTable.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\HeroBanner;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class HeroBannerDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', 'components.datatables.hero-banner.action-button')
+            ->addColumn('is_active', 'components.datatables.hero-banner.active-switch')
+            ->addColumn('media_preview', 'components.datatables.hero-banner.media-preview')
+            ->addIndexColumn()
+            ->setRowId('id')
+            ->rawColumns(['action', 'is_active', 'media_preview']);
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(HeroBanner $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+            ->setTableId('herobanner-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->dom('lfrtip')
+            ->orderBy(1)
+            ->selectStyleSingle();
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::computed('DT_RowIndex')->title('No')->orderable(false)->searchable(false),
+            Column::make('name'),
+            Column::make('description'),
+            Column::make('media_preview'),
+            Column::make('is_active'),
+            Column::computed('action'),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'HeroBanner_' . date('YmdHis');
+    }
+}
--- app/Http/Controllers/HeroBannerController.php
+++ app/Http/Controllers/HeroBannerController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTables\HeroBannerDataTable;
 use Illuminate\Http\Request;
 
 class HeroBannerController extends Controller
 {
-    public function index()
+    public function index(HeroBannerDataTable $dataTable)
     {
-        return 'index hero banner';
+        $title = 'Hero Banner List';
+
+        $data = [
+            'title' => $title
+        ];
+
+        return $dataTable->render("pages.admin.hero-banner.index", $data);
     }
 }
