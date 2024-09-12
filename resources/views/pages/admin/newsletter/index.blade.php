e715842739bf8cd0ad758a4c74e57c3f6c90a95e
fadelanumah <fadelanumah@gmail.com>
2024-09-12 15:30:51 +0700

    fix: Fix DataTable Not Responsive

--- resources/views/pages/admin/newsletter/index.blade.php
+++ resources/views/pages/admin/newsletter/index.blade.php
@@ -11,7 +11,7 @@
         <div class="col-12 col-lg-12">
             <div class="card">
                 <div class="card-header">Manage Newsletter</div>
-                <div class="card-body">
+                <div class="card-body table-responsive">
                     {{ $dataTable->table() }}
                     {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
                 </div>
